2739257d5086a24d74e180a39ea07743c84b9191
lokesh1989 <lokesh.lokesh.tammana@gmail.com>
2018-03-10 12:17:00 +0530

    111

---
+++ src/main/java/org/snapdeal/App.java
@@ -0,0 +1,13 @@
+package org.snapdeal;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
